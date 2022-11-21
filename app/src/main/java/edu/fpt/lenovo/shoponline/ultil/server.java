a1fc49352ed6214d647949475b0a655999b1d80a
tanlv <thonv1602@gmail.com>
2022-11-21 23:31:09 +0700

    quyle_commit create_base_app

--- app/src/main/java/edu/fpt/lenovo/shoponline/ultil/server.java
+++
@@ -1,8 +0,0 @@
-package edu.fpt.lenovo.shoponline.ultil;
-
-public class server {
-    public static String localhost = "192.168.1.3";
-    public static String Duongdanloaisp = "http://"+localhost+"/server/getloaisp.php";
-    public static String Duongdansanphammoinhat = "http://" + localhost + "/server/getsanphammoinhat.php";
-    public static String Duongdandienthoai = "http://" + localhost + "/server/getsanpham.php?page=";
-}
